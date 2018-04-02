2367fefcfa336ff4b5a660c52f7f143c80bec540
deadzone747 <deadzone747@gmail.com>
2018-04-02 18:27:38 +0200

    add unity + folder structers

---
+++ StatePatternSimpleExample/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+
+namespace StatePatternSimpleExample
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override System.Windows.DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
